45412320b3e791f5558ce8259b7e95f7149c6c1d
Tiago Silva <70734516+tiag188@users.noreply.github.com>
2022-11-23 00:20:00 -0300

    feat: desenvolver front

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const Home: React.FC = () => {
+    return (
+        <div className="container">
+            <h1>Gestar Vida</h1>
+        </div>
+    ) 
+}
+ 
+export default Home;
